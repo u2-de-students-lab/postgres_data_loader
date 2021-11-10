fe52fad7b9335bd5226e6a62ad8b10aa0d2915f8
oleg_gerasimovich <oleg-younost@mail.ru>
2021-11-10 23:06:10 +0300

    Fix code issuse

--- oleggerasimovich/parser.py
+++ oleggerasimovich/parser.py
@@ -4,31 +4,41 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 
-load_dotenv()
 
-db = os.getenv('DATABASE')
-password = os.getenv('PASSWORD')
-host = os.getenv('HOST')
-user = os.getenv('USER')
-port = os.getenv('PORT')
+def cli(user_csv: str):
 
-parser = argparse.ArgumentParser(description='Paste path to file')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user_csv', type=str, help='Put path to ur csv file')
+    cli_arguments  = parser.parse_args([user_csv])
 
-parser.add_argument('path', type=str, help='Put path to ur csv file')
-
-path = parser.parse_args()
-
-conn = psycopg2.connect(database=db, user=user, password=password, host=host, port=port)
-  
-cursor = conn.cursor()
-
-with open(path.path) as fl:
-    next(fl)
-    cursor.copy_expert("COPY train FROM STDIN WITH CSV", fl)
-
-conn.commit()
-conn.close()
+    return cli_arguments 
 
 
+def main():
+
+    user_csv = input('Paste path to your CSV file you want to parse: ')
+
+    path = cli(user_csv=user_csv)
+
+    load_dotenv()
+
+    db = os.environ.get('DATABASE')
+    password = os.environ.get('PASSWORD')
+    host = os.environ.get('HOST')
+    user = os.environ.get('USER')
+    port = os.environ.get('PORT')
+
+    conn = psycopg2.connect(database=db, user=user, password=password, host=host, port=port)
+
+    cursor = conn.cursor()
+
+    with open(path.user_csv) as fl:
+        next(fl)
+        cursor.copy_expert("COPY train FROM STDIN WITH CSV", fl)
+
+    conn.commit()
+    conn.close()
 
 
+if __name__ == '__main__':
+    main()
