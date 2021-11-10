aa6ca2868099bbc11290dc398889f972031f5564
oleg_gerasimovich <oleg-younost@mail.ru>
2021-11-10 18:17:52 +0300

    Add project structure

---
+++ oleggerasimovich/parser.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+db = os.getenv('DATABASE')
+password = os.getenv('PASSWORD')
+host = os.getenv('HOST')
+user = os.getenv('USER')
+port = os.getenv('PORT')
+
+parser = argparse.ArgumentParser(description='Paste path to file')
+
+parser.add_argument('path', type=str, help='Put path to ur csv file')
+
+path = parser.parse_args()
+
+conn = psycopg2.connect(database=db, user=user, password=password, host=host, port=port)
+
+# source .env - файлы среды, загружаются с помощью модуля os  
+
+cursor = conn.cursor()
+
+with open(path.path) as fl:
+    next(fl)
+    cursor.copy_expert("COPY train FROM STDIN WITH CSV", fl)
+
+conn.commit()
+conn.close()
+
+
+
+
